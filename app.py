ad1fa4cf181e10b7b1792ca14f60927cb8462afa
Will Holderness <will@unikey.com>
2016-04-10 02:38:38 -0400

    Added clickable links. Shrank images.

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def light_grid():
     items = [
-            {'class':'sabnzbd', 'img':'assets/sabnzbd.png', 'div':'sabdiv'},
-            {'class':'plexlogo', 'img':'assets/plex.png', 'div':'pldiv'},
-            {'class':'sickbeard', 'img':'assets/sickbeard.png', 'div':'sbdiv'},
-            {'class':'milight', 'img':'assets/light.png', 'div':'lidiv'} 
+            {'class':'sabnzbd', 'img':'assets/sabnzbd.png', 'div':'sabdiv', 'url':'https://home.holderness.io:10091'},
+            {'class':'plexlogo', 'img':'assets/plex.png', 'div':'pldiv', 'url':'http://home.holderness.io:32400/web'},
+            {'class':'sickbeard', 'img':'assets/sickbeard.png', 'div':'sbdiv', 'url':'https://home.holderness.io:10081'},
+            {'class':'milight', 'img':'assets/light.png', 'div':'lidiv', 'url':'http://home.holderness.io:8000'} 
             ];
     return render_template('index.html', items=items)
 
